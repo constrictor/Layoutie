f2ab8093a9ce18c8df06d3a2e8c310c23ea56cc2
Valentyn Pavliuchenko <valentyn.pavliuchenko@gmail.com>
2011-09-04 23:42:36 +0300

    Removed kdev4 project file.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+int main(int argc, char **argv)
+{
+    std::cout << "Hello, world! I'm layoutie!" << std::endl;
     return 0;
 }
